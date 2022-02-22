3cb9daf27039f17259493eb67ba64cd35e73a9e9
LoopFruits <Kensworldc@gmail.com>
2022-02-22 13:45:19 -0500

    Completed Assignmnet

--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -9,8 +9,16 @@ function ExampleComponent() {
         alt="a deeply impressed man, perhaps thinking 'whoa'"
       />
       <p>Whoa!</p>
+      {/* <img
+        src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif"
+        alt="a deeply impressed man, perhaps thinking 'whoa'"
+      />
+      <p>Whoa!</p> */} // example to show that you can make a second component to show in the dom/localhost
+      
     </div>
+    
   );
+  
 }
 
 export default ExampleComponent;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import "./index.css";
+import "./index.css"; // what we use to import something that is the default export of another file.
 
 ReactDOM.render(<App />, document.getElementById("root"));
